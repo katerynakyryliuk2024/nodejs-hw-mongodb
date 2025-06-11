1757d0b4d3c864eaac83fa6688d3551166ac2979
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-06-11 17:48:59 +0300

    update saveFileToCloudinary

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -9,10 +9,6 @@ cloudinary.v2.config({
   api_secret: getEnvVar('CLOUDINARY_API_SECRET'),
 });
 
-// export function uploadToCloudinary(filePath) {
-//   return cloudinary.v2.uploader.upload(filePath);
-// }
-
 export const saveFileToCloudinary = async (file) => {
   const response = await cloudinary.v2.uploader.upload(file.path);
   await fs.unlink(file.path);
