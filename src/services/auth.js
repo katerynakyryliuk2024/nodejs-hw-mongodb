e577aa9b161d60d741987aaacca1e06bf1adbfcb
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-06-16 14:54:02 +0300

    create folder docs

--- src/services/auth.js
+++ src/services/auth.js
@@ -176,7 +176,6 @@ export const loginOrSignupWithGoogle = async (code) => {
       email: payload.email,
       name: getFullNameFromGoogleTokenPayload(payload),
       password,
-      role: 'parent',
     });
   }
 
